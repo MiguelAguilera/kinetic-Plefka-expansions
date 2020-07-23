3888c447ef81095ae6f2e98d5cd72129b9369e29
MiguelAguilera <miguel@maguilera.net>
2020-07-23 13:33:21 +0200

    compare data and simulation

--- compare-T.py
+++ compare-T.py
@@ -6,206 +6,314 @@ Created on Wed Dec  5 15:45:08 2018
 @author: maguilera
 """
 
-from mf_ising import mf_ising, mf_ising_roudi
+from mf_ising import mf_ising
 from kinetic_ising import ising
 import numpy as np
 from matplotlib import pyplot as plt
-
+from matplotlib import cm
 
 plt.rc('text', usetex=True)
-font = {'size':15}
-plt.rc('font',**font)
-plt.rc('legend',**{'fontsize':12})
+font = {'size': 15}
+plt.rc('font', **font)
+plt.rc('legend', **{'fontsize': 12})
 
-size = 100
-beta = 0.9
-#beta = 1.2
-beta = 1.0
-R = 10000
+
+size = 512
+
+R = 500000
 mode = 'c'
-random_s0=False
-random_s0=True
+gamma1 = 0.5
+gamma2 = 0.1
 
-T = 32
-#T=8
-#T=2
+beta = 1.0
+
+
+T = 128
 iu1 = np.triu_indices(size, 1)
 
-mP0o2_mean = np.zeros(T)
-mP1o2_mean = np.zeros(T)
-mP1Co2_mean = np.zeros(T)
-mPexp_mean = np.zeros(T)
-mP0o2_std = np.zeros(T)
-mP1o2_std = np.zeros(T)
-mP1Co2_std = np.zeros(T)
-mP2o1_mean = np.zeros(T)
-mP2o2_mean = np.zeros(T)
-mP2o1_std = np.zeros(T)
-mP2o2_std = np.zeros(T)
-mPexp_std = np.zeros(T)
+EmP0o2 = np.zeros(T + 1)
+EmP1o2 = np.zeros(T + 1)
+EmP1Co2 = np.zeros(T + 1)
+EmP2o1 = np.zeros(T + 1)
 
-CP0o2_mean = np.zeros(T)
-CP1o2_mean = np.zeros(T)
-CP1Co2_mean = np.zeros(T)
-CPexp_mean = np.zeros(T)
-CP0o2_std = np.zeros(T)
-CP1o2_std = np.zeros(T)
-CP1Co2_std = np.zeros(T)
-CP2o1_mean = np.zeros(T)
-CP2o2_mean = np.zeros(T)
-CP2o1_std = np.zeros(T)
-CP2o2_std = np.zeros(T)
-CPexp_std = np.zeros(T)
+ECP0o2 = np.zeros(T + 1)
+ECP1o2 = np.zeros(T + 1)
+ECP1Co2 = np.zeros(T + 1)
+ECP2o1 = np.zeros(T + 1)
 
-DP0o2_mean = np.zeros(T)
-DP1o2_mean = np.zeros(T)
-DP1Co2_mean = np.zeros(T)
-DPexp_mean = np.zeros(T)
-DP0o2_std = np.zeros(T)
-DP1o2_std = np.zeros(T)
-DP1Co2_std = np.zeros(T)
-DP2o1_mean = np.zeros(T)
-DP2o2_mean = np.zeros(T)
-DP2o1_std = np.zeros(T)
-DP2o2_std = np.zeros(T)
-DPexp_std = np.zeros(T)
+EDP0o2 = np.zeros(T + 1)
+EDP1o2 = np.zeros(T + 1)
+EDP1Co2 = np.zeros(T + 1)
+EDP2o1 = np.zeros(T + 1)
 
-if random_s0:
-    filename = 'data/m-' + mode + '-rs0-s-' + str(size) + '-R-' + str(R) + '-beta-' + str(beta) + '.npz'
-else:
-    filename = 'data/m-' + mode + '-ss0-s-' + str(size) + '-R-' + str(R) + '-beta-' + str(beta) + '.npz'
+
+mP0o2_mean = np.ones(T + 1)
+mP1o2_mean = np.ones(T + 1)
+mP1Co2_mean = np.ones(T + 1)
+mPexp_mean = np.ones(T + 1)
+mP2o1_mean = np.ones(T + 1)
+mP0o2_std = np.zeros(T + 1)
+mP1o2_std = np.zeros(T + 1)
+mP1Co2_std = np.zeros(T + 1)
+mP2o1_std = np.zeros(T + 1)
+mPexp_std = np.zeros(T + 1)
+mP0o2_final = np.zeros(size)
+mP1o2_final = np.zeros(size)
+mP1Co2_final = np.zeros(size)
+mP2o1_final = np.zeros(size)
+mPexp_final = np.zeros(size)
+
+CP0o2_mean = np.zeros(T + 1)
+CP1o2_mean = np.zeros(T + 1)
+CP1Co2_mean = np.zeros(T + 1)
+CPexp_mean = np.zeros(T + 1)
+CP2o1_mean = np.zeros(T + 1)
+CP0o2_std = np.zeros(T + 1)
+CP1o2_std = np.zeros(T + 1)
+CP1Co2_std = np.zeros(T + 1)
+CP2o1_std = np.zeros(T + 1)
+CPexp_std = np.zeros(T + 1)
+CP0o2_final = np.zeros((size, size))
+CP1o2_final = np.zeros((size, size))
+CP1Co2_final = np.zeros((size, size))
+CP2o1_final = np.zeros((size, size))
+CPexp_final = np.zeros((size, size))
+
+DP0o2_mean = np.zeros(T + 1)
+DP1o2_mean = np.zeros(T + 1)
+DP1Co2_mean = np.zeros(T + 1)
+DPexp_mean = np.zeros(T + 1)
+DP2o1_mean = np.zeros(T + 1)
+DP0o2_std = np.zeros(T + 1)
+DP1o2_std = np.zeros(T + 1)
+DP1Co2_std = np.zeros(T + 1)
+DP2o1_std = np.zeros(T + 1)
+DPexp_std = np.zeros(T + 1)
+DP0o2_final = np.zeros((size, size))
+DP1o2_final = np.zeros((size, size))
+DP1Co2_final = np.zeros((size, size))
+DP2o1_final = np.zeros((size, size))
+DPexp_final = np.zeros((size, size))
+
+
+# Load data
+filename = 'data/m-c-ts0-gamma1-' + str(gamma1) + '-gamma2-' + str(gamma2) + '-s-' + \
+    str(size) + '-R-' + str(R) + '-beta-' + str(beta) + '.npz'
+print(filename)
 data = np.load(filename)
 H = data['H']
 J = data['J']
 s0 = data['s0']
-if not s0 is np.ndarray:
-    s0=None
-#	m_exp = data['m'][:, T]
-#	C_exp_T = data['C'][:, :, T]
-for t in range(T):
-	C_exp = data['C'][:, :, t+1]
-	CPexp_mean[t] = np.mean(C_exp[iu1])
-	CPexp_std[t] = np.std(C_exp[iu1])
 
-print('TAP')
+
+# Load statistical moments from data			
+mPexp_mean[0] = 1
+for t in range(T):
+    print('Exp', str(t) + '/' + str(T))
+    m_exp = data['m'][:, t]
+    mPexp_mean[t + 1] = np.mean(m_exp)
+    mPexp_std[t + 1] = np.std(m_exp)
+    C_exp = data['C'][:, :, t]
+    CPexp_mean[t + 1] = np.mean(C_exp[iu1])
+    CPexp_std[t + 1] = np.std(C_exp[iu1])
+    D_exp = data['D'][:, :, t]
+    DPexp_mean[t + 1] = np.mean(D_exp)
+    DPexp_std[t + 1] = np.std(D_exp)
+    print(mPexp_mean[t + 1], CPexp_mean[t + 1], DPexp_mean[t + 1])
+mPexp_final = m_exp
+CPexp_final = C_exp
+DPexp_final = D_exp
+
+
+# Initialize kinetic Ising model
 I = mf_ising(size)
 I.H = H.copy()
 I.J = J.copy()
 
+# Run Plefka[t-1,t], order 2
 I.initialize_state(s0)
 for t in range(T):
-	I.update_P0_o2()
-	mP0o2 = I.m.copy()
-	CP0o2 = I.C.copy()
-	DP0o2 = I.D.copy()
-	CP0o2_mean[t] = np.mean(CP0o2[iu1]-data['C'][:, :, t+1][iu1])
-	CP0o2_std[t] = np.std(CP0o2[iu1]-data['C'][:, :, t+1][iu1])
-	mP0o2_mean[t] = np.mean(mP0o2-data['m'][:, t+1])
-	mP0o2_std[t] = np.std(mP0o2-data['m'][:, t+1])
-	DP0o2_mean[t] = np.mean(DP0o2-data['D'][:,:, t+1])
-	DP0o2_std[t] = np.std(DP0o2-data['D'][:,:, t+1])
-	
+    print('P0_o2', str(t) + '/' + str(T))
+    I.update_P0_o2()
+    CP0o2_mean[t + 1] = np.mean(I.C[iu1])
+    CP0o2_std[t + 1] = np.std(I.C[iu1])
+    mP0o2_mean[t + 1] = np.mean(I.m)
+    mP0o2_std[t + 1] = np.std(I.m)
+    DP0o2_mean[t + 1] = np.mean(I.D)
+    DP0o2_std[t + 1] = np.std(I.D)
+    EmP0o2[t + 1] = np.mean((I.m - data['m'][:, t])**2)
+    ECP0o2[t + 1] = np.mean((I.C[iu1] - data['C'][:, :, t][iu1])**2)
+    EDP0o2[t + 1] = np.mean((I.D - data['D'][:, :, t])**2)
+    print(mP0o2_mean[t + 1], CP0o2_mean[t + 1], DP0o2_mean[t + 1])
+mP0o2_final = I.m
+CP0o2_final = I.C
+DP0o2_final = I.D
+
+# Run Plefka[t], order 2
 I.initialize_state(s0)
 for t in range(T):
-	I.update_P1_o2()
-	mP1o2 = I.m.copy()
-	CP1o2 = I.C.copy()
-	DP1o2 = I.D.copy()
-	CP1o2_mean[t] = np.mean(CP1o2[iu1]-data['C'][:, :, t+1][iu1])
-	CP1o2_std[t] = np.std(CP1o2[iu1]-data['C'][:, :, t+1][iu1])
-	mP1o2_mean[t] = np.mean(mP1o2-data['m'][:, t+1])
-	mP1o2_std[t] = np.std(mP1o2-data['m'][:, t+1])
-	DP1o2_mean[t] = np.mean(DP1o2-data['D'][:,:, t+1])
-	DP1o2_std[t] = np.std(DP1o2-data['D'][:,:, t+1])
-	
-	
+    print('P1_o2', str(t) + '/' + str(T))
+    I.update_P1_o2()
+    CP1o2_mean[t + 1] = np.mean(I.C[iu1])
+    CP1o2_std[t + 1] = np.std(I.C[iu1])
+    mP1o2_mean[t + 1] = np.mean(I.m)
+    mP1o2_std[t + 1] = np.std(I.m)
+    DP1o2_mean[t + 1] = np.mean(I.D)
+    DP1o2_std[t + 1] = np.std(I.D)
+    EmP1o2[t + 1] = np.mean((I.m - data['m'][:, t])**2)
+    ECP1o2[t + 1] = np.mean((I.C[iu1] - data['C'][:, :, t][iu1])**2)
+    EDP1o2[t + 1] = np.mean((I.D - data['D'][:, :, t])**2)
+    print(mP1o2_mean[t + 1], CP1o2_mean[t + 1], DP1o2_mean[t + 1])
+mP1o2_final = I.m
+CP1o2_final = I.C
+DP1o2_final = I.D
+
+# Run Plefka2[t], order 2
 I.initialize_state(s0)
 for t in range(T):
-	I.update_P1C_o2()
-	mP1Co2 = I.m.copy()
-	CP1Co2 = I.C.copy()
-	DP1Co2 = I.D.copy()
-	CP1Co2_mean[t] = np.mean(CP1Co2[iu1]-data['C'][:, :, t+1][iu1])
-	CP1Co2_std[t] = np.std(CP1Co2[iu1]-data['C'][:, :, t+1][iu1])
-	mP1Co2_mean[t] = np.mean(mP1Co2-data['m'][:, t+1])
-	mP1Co2_std[t] = np.std(mP1Co2-data['m'][:, t+1])
-	DP1Co2_mean[t] = np.mean(DP1Co2-data['D'][:,:, t+1])
-	DP1Co2_std[t] = np.std(DP1Co2-data['D'][:,:, t+1])
+    print('P1C_o2', str(t) + '/' + str(T))
+    I.update_P1C_o2()
+    CP1Co2_mean[t + 1] = np.mean(I.C[iu1])
+    CP1Co2_std[t + 1] = np.std(I.C[iu1])
+    mP1Co2_mean[t + 1] = np.mean(I.m)
+    mP1Co2_std[t + 1] = np.std(I.m)
+    DP1Co2_mean[t + 1] = np.mean(I.D)
+    DP1Co2_std[t + 1] = np.std(I.D)
+    EmP1Co2[t + 1] = np.mean((I.m - data['m'][:, t])**2)
+    ECP1Co2[t + 1] = np.mean((I.C[iu1] - data['C'][:, :, t][iu1])**2)
+    EDP1Co2[t + 1] = np.mean((I.D - data['D'][:, :, t])**2)
+    print(mP1Co2_mean[t + 1], CP1Co2_mean[t + 1], DP1Co2_mean[t + 1])
+mP1Co2_final = I.m
+CP1Co2_final = I.C
+DP1Co2_final = I.D
 
-	
+# Run Plefka[t-1], order 1
 I.initialize_state(s0)
 for t in range(T):
-	I.update_P2_o1()
-	mP2o1 = I.m.copy()
-	CP2o1 = I.C.copy()
-	DP2o1 = I.D.copy()
-	CP2o1_mean[t] = np.mean(CP2o1[iu1]-data['C'][:, :, t+1][iu1])
-	CP2o1_std[t] = np.std(CP2o1[iu1]-data['C'][:, :, t+1][iu1])
-	mP2o1_mean[t] = np.mean(mP2o1-data['m'][:, t+1])
-	mP2o1_std[t] = np.std(mP2o1-data['m'][:, t+1])
-	DP2o1_mean[t] = np.mean(DP2o1[iu1]-data['D'][:, :, t+1])
-	DP2o1_std[t] = np.std(DP2o1[iu1]-data['D'][:, :, t+1])
-#I.initialize_state(s0)
-#for t in range(T):
-#	I.update_P2_o2()
-#	mP2o2 = I.m.copy()
-#	CP2o2 = I.C.copy()
-#	P2o2_mean[t] = np.mean(CP2o2[iu1]-data['C'][:, :, t+1][iu1])
-#	P2o2_std[t] = np.std(CP2o2[iu1]-data['C'][:, :, t+1][iu1])
+    print('P2_o1', str(t) + '/' + str(T))
+    I.update_P2_o1()
+    CP2o1_mean[t + 1] = np.mean(I.C[iu1])
+    CP2o1_std[t + 1] = np.std(I.C[iu1])
+    mP2o1_mean[t + 1] = np.mean(I.m)
+    mP2o1_std[t + 1] = np.std(I.m)
+    DP2o1_mean[t + 1] = np.mean(I.D)
+    DP2o1_std[t + 1] = np.std(I.D)
+    EmP2o1[t + 1] = np.mean((I.m - data['m'][:, t])**2)
+    ECP2o1[t + 1] = np.mean((I.C[iu1] - data['C'][:, :, t][iu1])**2)
+    EDP2o1[t + 1] = np.mean((I.D - data['D'][:, :, t])**2)
+mP2o1_final = I.m
+CP2o1_final = I.C
+DP2o1_final = I.D
+
+# Save results to file
+
+filename = 'img/compare-T_' + str(int(beta * 100)) + '_size_'+str(size)+'.npz'
+np.savez_compressed(filename,
+                    m_exp=data['m'][:, t], C_exp=data['C'][:,
+                                                           :, t], D_exp=data['D'][:, :, t],
+                    mP0o2_mean=mP0o2_mean, mP1o2_mean=mP1o2_mean, mP2o1_mean=mP2o1_mean, mP1Co2_mean=mP1Co2_mean,
+                    CP0o2_mean=CP0o2_mean, CP1o2_mean=CP1o2_mean, CP2o1_mean=CP2o1_mean, CP1Co2_mean=CP1Co2_mean,
+                    DP0o2_mean=DP0o2_mean, DP1o2_mean=DP1o2_mean, DP2o1_mean=DP2o1_mean, DP1Co2_mean=DP1Co2_mean,
+                    mP0o2_std=mP0o2_std, mP1o2_std=mP1o2_std, mP2o1_std=mP2o1_std, mP1Co2_std=mP1Co2_std,
+                    CP0o2_std=CP0o2_std, CP1o2_std=CP1o2_std, CP2o1_std=CP2o1_std, CP1Co2_std=CP1Co2_std,
+                    DP0o2_std=DP0o2_std, DP1o2_std=DP1o2_std, DP2o1_std=DP2o1_std, DP1Co2_std=DP1Co2_std,
+                    mPexp_mean=mPexp_mean, CPexp_mean=CPexp_mean, DPexp_mean=DPexp_mean,
+                    mP0o2=mP0o2_final, mP1o2=mP1o2_final, mP2o1=mP2o1_final, mP1Co2=mP1Co2_final,
+                    CP0o2=CP0o2_final, CP1o2=CP1o2_final, CP2o1=CP2o1_final, CP1Co2=CP1Co2_final,
+                    DP0o2=DP0o2_final, DP1o2=DP1o2_final, DP2o1=DP2o1_final, DP1Co2=DP1Co2_final,
+                    EmP0o2=EmP0o2, EmP1o2=EmP1o2, EmP2o1=EmP2o1, EmP1Co2=EmP1Co2,
+                    ECP0o2=ECP0o2, ECP1o2=ECP1o2, ECP2o1=ECP2o1, ECP1Co2=ECP1Co2,
+                    EDP0o2=EDP0o2, EDP1o2=EDP1o2, EDP2o1=EDP2o1, EDP1Co2=EDP1Co2)
 
 
-steps=np.arange(T)+1
+# Plot results
 
-fig, ax = plt.subplots(1,1,figsize=(5,4))
-plt.plot(steps,mP0o2_mean,'b',label='P[t-1:t]')
-plt.fill_between(steps,mP0o2_mean-mP0o2_std,mP0o2_mean+mP0o2_std, color='b',alpha=0.25)
-plt.plot(steps,mP1o2_mean,'g',label='P[t]')
-plt.fill_between(steps,mP1o2_mean-mP1o2_std,mP1o2_mean+mP1o2_std, color='g',alpha=0.25)
-plt.plot(steps,mP1Co2_mean,'r',label='P2[C]')
-plt.fill_between(steps,mP1Co2_mean-mP1Co2_std,mP1Co2_mean+mP1Co2_std, color='r',alpha=0.25)
-plt.plot(steps,mP2o1_mean,'c',label='P[t-1]')
-plt.fill_between(steps,mP2o1_mean-mP2o1_std,mP2o1_mean+mP2o1_std, color='c',alpha=0.25)
-plt.plot(steps,steps*0,'k')
-plt.title(r'$\beta='+str(beta)+r'$')
-plt.xlabel(r'$t$')
-plt.ylabel(r'$\epsilon_C$')
+steps = np.arange(T + 1)
+
+cmap = cm.get_cmap('inferno_r')
+colors = []
+for i in range(4):
+    colors += [cmap((i+0.5)/4)]
+labels = [r'P[$t-1,t$]', r'P[$t$]', r'P[$t-1$]', r'P2[$t$]']
+
+
+fig, ax = plt.subplots(1, 1, figsize=(5, 4))
+plt.plot(steps, EmP0o2, 'b', label=labels[0])
+plt.plot(steps, EmP1o2, 'g', label=labels[1])
+plt.plot(steps, EmP2o1, 'm', label=labels[2])
+plt.plot(steps, EmP1Co2, 'r', label=labels[3])
+plt.title(r'$\beta/\beta_c=' + str(beta) + r'$', fontsize=18)
+plt.xlabel(r'$t$', fontsize=18)
+plt.ylabel(r'$MSE[\textbf{m}_t]$', fontsize=18, rotation=0, labelpad=30)
 plt.legend()
+# plt.savefig('img/error_m-beta_' + str(int(beta * 10)) +
+#            '.pdf', bbox_inches='tight')
 
 
-
-fig, ax = plt.subplots(1,1,figsize=(5,4))
-plt.plot(steps,CP0o2_mean,'b',label='P[t-1:t]')
-plt.fill_between(steps,CP0o2_mean-CP0o2_std,CP0o2_mean+CP0o2_std, color='b',alpha=0.25)
-plt.plot(steps,CP1o2_mean,'g',label='P[t]')
-plt.fill_between(steps,CP1o2_mean-CP1o2_std,CP1o2_mean+CP1o2_std, color='g',alpha=0.25)
-plt.plot(steps,CP1Co2_mean,'r',label='P2[C]')
-plt.fill_between(steps,CP1Co2_mean-CP1Co2_std,CP1Co2_mean+CP1Co2_std, color='r',alpha=0.25)
-plt.plot(steps,CP2o1_mean,'c',label='P[t-1]')
-plt.fill_between(steps,CP2o1_mean-CP2o1_std,CP2o1_mean+CP2o1_std, color='c',alpha=0.25)
-#plt.plot(steps,P2o2_mean,'m',label='P[t-1]')
-#plt.fill_between(steps,P2o2_mean-P2o2_std,P2o2_mean+P2o2_std, color='m',alpha=0.25)
-plt.plot(steps,steps*0,'k')
-plt.title(r'$\beta='+str(beta)+r'$')
-plt.xlabel(r'$t$')
-plt.ylabel(r'$\epsilon_C$')
+fig, ax = plt.subplots(1, 1, figsize=(5, 4))
+plt.plot(steps, ECP0o2, 'b', label=labels[0])
+plt.plot(steps, ECP1o2, 'g', label=labels[1])
+plt.plot(steps, ECP2o1, 'm', label=labels[2])
+plt.plot(steps, ECP1Co2, 'r', label=labels[3])
+plt.title(r'$\beta/\beta_c=' + str(beta) + r'$', fontsize=18)
+plt.xlabel(r'$t$', fontsize=18)
+plt.ylabel(r'$MSE[\textbf{C}_t]$', fontsize=18, rotation=0, labelpad=30)
 plt.legend()
+# plt.savefig('img/error_C-beta_' + str(int(beta * 10)) +
+#            '.pdf', bbox_inches='tight')
 
-
-fig, ax = plt.subplots(1,1,figsize=(5,4))
-plt.plot(steps,DP0o2_mean,'b',label='P[t-1:t]')
-plt.fill_between(steps,DP0o2_mean-DP0o2_std,DP0o2_mean+DP0o2_std, color='b',alpha=0.25)
-plt.plot(steps,DP1o2_mean,'g',label='P[t]')
-plt.fill_between(steps,DP1o2_mean-DP1o2_std,DP1o2_mean+DP1o2_std, color='g',alpha=0.25)
-plt.plot(steps,DP1Co2_mean,'r',label='P2[D]')
-plt.fill_between(steps,DP1Co2_mean-DP1Co2_std,DP1Co2_mean+DP1Co2_std, color='r',alpha=0.25)
-plt.plot(steps,DP2o1_mean,'c',label='P[t-1]')
-plt.fill_between(steps,DP2o1_mean-DP2o1_std,DP2o1_mean+DP2o1_std, color='c',alpha=0.25)
-plt.plot(steps,steps*0,'k')
-plt.title(r'$\beta='+str(beta)+r'$')
-plt.xlabel(r'$t$')
-plt.ylabel(r'$\epsilon_D$')
+fig, ax = plt.subplots(1, 1, figsize=(5, 4))
+plt.plot(steps, EDP0o2, 'b', label=labels[0])
+plt.plot(steps, EDP1o2, 'g', label=labels[1])
+plt.plot(steps, EDP2o1, 'm', label=labels[2])
+plt.plot(steps, EDP1Co2, 'r', label=r'P[D]')
+plt.title(r'$\beta/\beta_c=' + str(beta) + r'$', fontsize=18)
+plt.xlabel(r'$t$', fontsize=18)
+plt.ylabel(r'$MSE[\textbf{D}_t]$', fontsize=18, rotation=0, labelpad=30)
 plt.legend()
+# plt.savefig('img/error_D-beta_' + str(int(beta * 10)) +
+#            '.pdf', bbox_inches='tight')
 
+fig, ax = plt.subplots(1, 1, figsize=(5, 4))
+plt.plot(steps, mP0o2_mean, 'v', color=colors[0], ms=3, label=labels[0])
+plt.plot(steps, mP1o2_mean, 's', color=colors[1], ms=3, label=labels[1])
+plt.plot(steps, mP2o1_mean, 'd', color=colors[2], ms=3, label=labels[2])
+plt.plot(steps, mP1Co2_mean, 'o', color=colors[3], ms=3, label=labels[3])
+plt.plot(steps, mPexp_mean, 'k', label=r'$P$')
+plt.title(r'$\beta/\beta_c=' + str(beta) + r'$', fontsize=18)
+plt.xlabel(r'$t$', fontsize=18)
+plt.ylabel(r'$\langle m_{i,t} \rangle$', fontsize=18, rotation=0, labelpad=15)
+plt.legend()
+plt.savefig('img/evolution_m-beta_' + str(int(beta * 100)) +
+            '.pdf', bbox_inches='tight')
+
+
+fig, ax = plt.subplots(1, 1, figsize=(5, 4))
+plt.plot(steps, CP0o2_mean, 'v', color=colors[0], ms=3, label=labels[0])
+plt.plot(steps, CP1o2_mean, 's', color=colors[1], ms=3, label=labels[1])
+plt.plot(steps, CP2o1_mean, 'd', color=colors[2], ms=3, label=labels[2])
+plt.plot(steps, CP1Co2_mean, 'o', color=colors[3], ms=3, label=labels[3])
+plt.plot(steps, CPexp_mean, 'k', label=r'$P$')
+plt.title(r'$\beta/\beta_c=' + str(beta) + r'$', fontsize=18)
+plt.xlabel(r'$t$', fontsize=18)
+plt.ylabel(r'$\langle C_{ik,t} \rangle$', fontsize=18, rotation=0, labelpad=15)
+plt.legend(loc='lower right')
+# plt.axis([0,T,0,1])
+plt.savefig('img/evolution_C-beta_' + str(int(beta * 100)) +
+            '.pdf', bbox_inches='tight')
+
+fig, ax = plt.subplots(1, 1, figsize=(5, 4))
+plt.plot(steps, DP0o2_mean, 'v', color=colors[0], ms=3, label=labels[0])
+plt.plot(steps, DP1o2_mean, 's', color=colors[1], ms=3, label=labels[1])
+plt.plot(steps, DP2o1_mean, 'd', color=colors[2], ms=3, label=labels[2])
+plt.plot(steps, DP1Co2_mean, 'o', color=colors[3], ms=3, label=labels[3])
+plt.plot(steps, DPexp_mean, 'k', label=r'$P$')
+plt.title(r'$\beta/\beta_c=' + str(beta) + r'$', fontsize=18)
+plt.xlabel(r'$t$', fontsize=18)
+plt.ylabel(r'$\langle D_{il,t} \rangle$', fontsize=18, rotation=0, labelpad=15)
+plt.legend(loc='lower right')
+# plt.axis([0,T,0,1])
+plt.savefig('img/evolution_D-beta_' + str(int(beta * 100)) +
+            '.pdf', bbox_inches='tight')
 
 plt.show()
-
