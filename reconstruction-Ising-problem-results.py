b5b0841f4dc2a777422a3ee5569a290a1e41c69f
MiguelAguilera <miguel@maguilera.net>
2020-10-01 18:08:13 +0200

    reconstruction problem

---
+++ reconstruction-Ising-problem-results.py
@@ -0,0 +1,322 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec  5 15:45:08 2018
+
+@author: maguilera
+"""
+
+from mf_ising import mf_ising
+import numpy as np
+from matplotlib import pyplot as plt
+from matplotlib import cm
+
+plt.rc('text', usetex=True)
+font = {'size': 15}
+plt.rc('font', **font)
+plt.rc('legend', **{'fontsize': 18})
+
+
+size = 512
+R = 1000000
+R1 = 10000
+gamma1 = 0.5
+gamma2 = 0.1
+
+T = 128
+iu1 = np.triu_indices(size, 1)
+
+offset = np.arange(3)
+width = 0.20
+
+B = 201
+betas = 1 + np.linspace(-1, 1, B) * 0.3
+
+N = len(betas)
+M = 4
+errorm = np.zeros((M, N))
+errorC = np.zeros((M, N))
+errorD = np.zeros((M, N))
+errorH = np.zeros((M, N))
+errorJ = np.zeros((M, N))
+
+mPexp = np.zeros(B)
+mP_t1_t = np.zeros(B)
+mP_t = np.zeros(B)
+mP_t1 = np.zeros(B)
+mP2_t = np.zeros(B)
+mP_t1_t_0 = np.zeros(B)
+mP_t_0 = np.zeros(B)
+mP_t1_0 = np.zeros(B)
+mP2_t_0 = np.zeros(B)
+emP_t1_t = np.zeros(B)
+emP_t = np.zeros(B)
+emP_t1 = np.zeros(B)
+emP2_t = np.zeros(B)
+
+CPexp = np.zeros(B)
+CP_t1_t = np.zeros(B)
+CP_t = np.zeros(B)
+CP_t1 = np.zeros(B)
+CP2_t = np.zeros(B)
+CP_t1_t_0 = np.zeros(B)
+CP_t_0 = np.zeros(B)
+CP_t1_0 = np.zeros(B)
+CP2_t_0 = np.zeros(B)
+eCP_t1_t = np.zeros(B)
+eCP_t = np.zeros(B)
+eCP_t1 = np.zeros(B)
+eCP2_t = np.zeros(B)
+
+DPexp = np.zeros(B)
+DP_t1_t = np.zeros(B)
+DP_t = np.zeros(B)
+DP_t1 = np.zeros(B)
+DP2_t = np.zeros(B)
+DP_t1_t_0 = np.zeros(B)
+DP_t_0 = np.zeros(B)
+DP_t1_0 = np.zeros(B)
+DP2_t_0 = np.zeros(B)
+eDP_t1_t = np.zeros(B)
+eDP_t = np.zeros(B)
+eDP_t1 = np.zeros(B)
+eDP2_t = np.zeros(B)
+
+sigmaPexp = np.zeros(B)
+sigmaP_t1_t = np.zeros(B)
+sigmaP_t = np.zeros(B)
+sigmaP_t1 = np.zeros(B)
+sigmaP2_t = np.zeros(B)
+
+sigmaP_t1_t_0 = np.zeros(B)
+sigmaP_t_0 = np.zeros(B)
+sigmaP_t1_0 = np.zeros(B)
+sigmaP2_t_0 = np.zeros(B)
+
+filename_exp = 'data/reconstruction/data-transition-gamma1-0.5-gamma2-0.1-s-512-R-10000-B-201.npz'
+data_exp = np.load(filename_exp)
+print(list(data_exp.keys()))
+mPexp = data_exp['m_mean']
+CPexp = data_exp['C_mean']
+DPexp = data_exp['D_mean']
+sigmaPexp = np.exp(data_exp['sigma'])
+
+filename_inv = 'data/results/inverse_100_R_' + str(R) + '.npz'
+data_inv = np.load(filename_inv)
+
+J = data_inv['J']
+JP_t1_t = data_inv['JP_t1_t']
+JP_t = data_inv['JP_t']
+JP_t1 = data_inv['JP_t1']
+JP2_t = data_inv['JP2_t']
+
+for ib in range(len(betas)):
+    beta_ref = round(betas[ib], 4)
+    print(ib, beta_ref)
+    filename_r = 'data/reconstruction/transition_r_' + \
+        str(int(round(beta_ref * 1000))) + '_R_' + str(R) + '.npz'
+    filename_d = 'data/reconstruction/transition_d_' + \
+        str(int(round(beta_ref * 1000))) + '_R_' + str(R) + '.npz'
+
+    data_r = np.load(filename_r)
+    data_d = np.load(filename_d)
+
+    mP_t1_t_all = data_r['mP_t1_t']
+    mP_t_all = data_r['mP_t']
+    mP2_t_all = data_r['mP2_t']
+    mP_t1_all = data_r['mP_t1']
+    CP_t1_t_all = data_r['CP_t1_t']
+    CP_t_all = data_r['CP_t']
+    CP2_t_all = data_r['CP2_t']
+    CP_t1_all = data_r['CP_t1']
+    DP_t1_t_all = data_r['DP_t1_t']
+    DP_t_all = data_r['DP_t']
+    DP2_t_all = data_r['DP2_t']
+    DP_t1_all = data_r['DP_t1']
+
+    mP_t1_t_all_0 = data_d['mP_t1_t']
+    mP_t_all_0 = data_d['mP_t']
+    mP2_t_all_0 = data_d['mP2_t']
+    mP_t1_all_0 = data_d['mP_t1']
+    CP_t1_t_all_0 = data_d['CP_t1_t']
+    CP_t_all_0 = data_d['CP_t']
+    CP2_t_all_0 = data_d['CP2_t']
+    CP_t1_all_0 = data_d['CP_t1']
+    DP_t1_t_all_0 = data_d['DP_t1_t']
+    DP_t_all_0 = data_d['DP_t']
+    DP2_t_all_0 = data_d['DP2_t']
+    DP_t1_all_0 = data_d['DP_t1']
+
+    sigmaP_t1_t[ib] = np.exp(
+        np.sum(JP_t1_t * beta_ref * (DP_t1_t_all - DP_t1_t_all.T)))
+    sigmaP_t[ib] = np.exp(np.sum(JP_t * beta_ref * (DP_t_all - DP_t_all.T)))
+    sigmaP_t1[ib] = np.exp(
+        np.sum(JP_t1 * beta_ref * (DP_t1_all - DP_t1_all.T)))
+    sigmaP2_t[ib] = np.exp(
+        np.sum(JP2_t * beta_ref * (DP2_t_all - DP2_t_all.T)))
+
+    sigmaP_t1_t_0[ib] = np.exp(
+        np.sum(J * beta_ref * (DP_t1_t_all_0 - DP_t1_t_all_0.T)))
+    sigmaP_t_0[ib] = np.exp(np.sum(J * beta_ref * (DP_t_all_0 - DP_t_all_0.T)))
+    sigmaP_t1_0[ib] = np.exp(
+        np.sum(J * beta_ref * (DP_t1_all_0 - DP_t1_all_0.T)))
+    sigmaP2_t_0[ib] = np.exp(
+        np.sum(J * beta_ref * (DP2_t_all_0 - DP2_t_all_0.T)))
+
+    mP_t1_t[ib] = np.mean(mP_t1_t_all)
+    mP_t[ib] = np.mean(mP_t_all)
+    mP_t1[ib] = np.mean(mP_t1_all)
+    mP2_t[ib] = np.mean(mP2_t_all)
+    CP_t1_t[ib] = np.mean(CP_t1_t_all[iu1])
+    CP_t[ib] = np.mean(CP_t_all[iu1])
+    CP_t1[ib] = np.mean(CP_t1_all[iu1])
+    CP2_t[ib] = np.mean(CP2_t_all[iu1])
+    DP_t1_t[ib] = np.mean(DP_t1_t_all)
+    DP_t[ib] = np.mean(DP_t_all)
+    DP_t1[ib] = np.mean(DP_t1_all)
+    DP2_t[ib] = np.mean(DP2_t_all)
+
+    mP_t1_t_0[ib] = np.mean(mP_t1_t_all_0)
+    mP_t_0[ib] = np.mean(mP_t_all_0)
+    mP_t1_0[ib] = np.mean(mP_t1_all_0)
+    mP2_t_0[ib] = np.mean(mP2_t_all_0)
+    CP_t1_t_0[ib] = np.mean(CP_t1_t_all_0[iu1])
+    CP_t_0[ib] = np.mean(CP_t_all_0[iu1])
+    CP_t1_0[ib] = np.mean(CP_t1_all_0[iu1])
+    CP2_t_0[ib] = np.mean(CP2_t_all_0[iu1])
+    DP_t1_t_0[ib] = np.mean(DP_t1_t_all_0)
+    DP_t_0[ib] = np.mean(DP_t_all_0)
+    DP_t1_0[ib] = np.mean(DP_t1_all_0)
+    DP2_t_0[ib] = np.mean(DP2_t_all_0)
+    del data_r
+    del data_d
+
+
+labels = [
+    r'Plefka[$t-1,t$]',
+    r'Plefka[$t$]',
+    r'Plefka[$t-1$]',
+    r'Plefka2[$t$]',
+    r'Original']
+line = [(5, 4), (5, 4), '', '']
+cmap = cm.get_cmap('plasma_r')
+colors = []
+for i in range(4):
+    colors += [cmap((i) / 3)]
+lws = [2, 2, 2, 3, 1.5]
+pos_l = [-0.2, 1.0]
+
+letters = ['A', 'B', 'C', 'D', 'E', 'F']
+
+fig, ax = plt.subplots(2, 3, figsize=(16, 10 * 2 / 3), dpi=300)
+
+nrow = 0
+ncol = 0
+ax[nrow, ncol].plot(betas, CPexp, color='k')
+ax[nrow, ncol].plot(betas, CP_t1_0, dashes=line[2], color=colors[2], lw=lws[2])
+ax[nrow, ncol].plot(betas, CP2_t_0, dashes=line[3], color=colors[3], lw=lws[3])
+ax[nrow, ncol].plot(betas, CP_t1_t_0, dashes=line[0], color=colors[0], lw=lws[0])
+ax[nrow, ncol].plot(betas, CP_t_0, dashes=line[1], color=colors[1], lw=lws[1])
+ax[nrow, ncol].plot(betas[np.argmax(CPexp)], [0.019], '*', ms=12, color='k')
+ax[nrow, ncol].plot(betas[np.argmax(CP_t1_t_0)], [ 0.019], '*', ms=12, color=colors[0])
+ax[nrow, ncol].plot(betas[np.argmax(CP_t_0)], [0.021], '*', ms=12, color=colors[1])
+ax[nrow, ncol].plot(betas[np.argmax(CP_t1_0)], [0.019], '*', ms=12, color=colors[2])
+ax[nrow, ncol].plot(betas[np.argmax(CP2_t_0)], [0.019], '*', ms=12, color=colors[3])
+ax[nrow, ncol].axis([np.min(betas), np.max(betas), 0, 0.022])
+ax[nrow, ncol].set_xlabel(r'$\beta / \beta_c$', fontsize=18)
+ax[nrow, ncol].set_ylabel(r'$\langle C_{ik,t} \rangle$', fontsize=18, rotation=0, labelpad=25)
+ax[nrow, ncol].text(pos_l[0], pos_l[1], r'\textbf ' + letters[0], transform=ax[nrow, ncol].transAxes, fontsize=20, va='top', ha='right')
+
+nrow = 0
+ncol = 1
+ax[nrow, ncol].plot(betas, DPexp, color='k')
+ax[nrow, ncol].plot(betas, DP_t1_0, dashes=line[2], color=colors[2], lw=lws[2])
+ax[nrow, ncol].plot(betas, DP2_t_0, dashes=line[3], color=colors[3], lw=lws[3])
+ax[nrow, ncol].plot(betas, DP_t1_t_0, dashes=line[0], color=colors[0], lw=lws[0])
+ax[nrow, ncol].plot(betas, DP_t_0, dashes=line[1], color=colors[1], lw=lws[1])
+ax[nrow, ncol].plot(betas[np.argmax(DPexp)], [0.019], '*', ms=12, color='k')
+ax[nrow, ncol].plot(betas[np.argmax(DP_t1_t_0)], [ 0.019], '*', ms=12, color=colors[0])
+ax[nrow, ncol].plot(betas[np.argmax(DP_t_0)], [0.021], '*', ms=12, color=colors[1])
+ax[nrow, ncol].plot(betas[np.argmax(DP_t1_0)], [0.019], '*', ms=12, color=colors[2])
+ax[nrow, ncol].plot(betas[np.argmax(DP2_t_0)], [0.019], '*', ms=12, color=colors[3])
+ax[nrow, ncol].axis([np.min(betas), np.max(betas), 0, 0.022])
+ax[nrow, ncol].set_xlabel(r'$\beta / \beta_c$', fontsize=18)
+ax[nrow, ncol].set_ylabel(r'$\langle D_{il,t} \rangle$', fontsize=18, rotation=0, labelpad=25)
+ax[nrow, ncol].text(pos_l[0], pos_l[1], r'\textbf ' + letters[1], transform=ax[nrow, ncol].transAxes, fontsize=20, va='top', ha='right')
+
+nrow = 0
+ncol = 2
+
+ax[nrow, ncol].plot(betas, sigmaPexp, color='k', label=labels[4])
+ax[nrow, ncol].plot(betas, sigmaP_t1_t_0, dashes=line[0], color=colors[0], lw=lws[0], label=labels[0])
+ax[nrow, ncol].plot(betas, sigmaP_t_0, dashes=line[1], color=colors[1], lw=lws[1], label=labels[1])
+ax[nrow, ncol].plot(betas, sigmaP_t1_0, dashes=line[2], color=colors[2], lw=lws[2], label=labels[2])
+ax[nrow, ncol].plot(betas, sigmaP2_t_0, dashes=line[3], color=colors[3], lw=lws[3], label=labels[3])
+ax[nrow, ncol].plot(betas, sigmaPexp, color='k')
+ax[nrow, ncol].plot(betas, sigmaP_t1_t_0, dashes=line[0], color=colors[0], lw=lws[0])
+ax[nrow, ncol].plot(betas, sigmaP_t1_0, dashes=line[2], color=colors[2], lw=lws[2])
+ax[nrow, ncol].plot(betas[np.argmax(sigmaPexp)], [110], '*', ms=12, color='k')
+ax[nrow, ncol].plot(betas[np.argmax(sigmaP_t1_t_0)], [ 130], '*', ms=12, color=colors[0])
+ax[nrow, ncol].plot(betas[np.argmax(sigmaP_t_0)], [ 130], '*', ms=12, color=colors[1])
+ax[nrow, ncol].plot(betas[np.argmax(sigmaP_t1_0)], [ 110], '*', ms=12, color=colors[2])
+ax[nrow, ncol].plot(betas[np.argmax(sigmaP2_t_0)], [ 120], '*', ms=12, color=colors[3])
+ax[nrow, ncol].axis([np.min(betas), np.max(betas), 0, 135])
+ax[nrow, ncol].set_xlabel(r'$\beta / \beta_c$', fontsize=18)
+ax[nrow, ncol].set_ylabel(r'$\mathrm{e}^{\sigma_t}$', fontsize=18, rotation=0, labelpad=25)
+ax[nrow, ncol].text(pos_l[0], pos_l[1], r'\textbf ' + letters[5], transform=ax[nrow, ncol].transAxes, fontsize=20, va='top', ha='right')
+
+nrow = 1
+ncol = 0
+ax[nrow, ncol].plot(betas, CPexp, color='k')
+ax[nrow, ncol].plot(betas, CP_t1, dashes=line[2], color=colors[2], lw=lws[2])
+ax[nrow, ncol].plot(betas, CP2_t, dashes=line[3], color=colors[3], lw=lws[3])
+ax[nrow, ncol].plot(betas, CP_t1_t, dashes=line[0], color=colors[0], lw=lws[0])
+ax[nrow, ncol].plot(betas, CP_t, dashes=line[1], color=colors[1], lw=lws[1])
+ax[nrow, ncol].plot(betas[np.argmax(CPexp)], [0.017], '*', ms=12, color='k')
+ax[nrow, ncol].plot(betas[np.argmax(CP_t1_t)], [0.017], '*', ms=12, color=colors[0])
+ax[nrow, ncol].plot(betas[np.argmax(CP_t)], [0.017], '*', ms=12, color=colors[1])
+ax[nrow, ncol].plot(betas[np.argmax(CP_t1)], [0.019], '*', ms=12, color=colors[2])
+ax[nrow, ncol].plot(betas[np.argmax(CP2_t)], [0.021], '*', ms=12, color=colors[3])
+ax[nrow, ncol].axis([np.min(betas), np.max(betas), 0, 0.022])
+ax[nrow, ncol].set_xlabel(r'$\tilde\beta$', fontsize=18)
+ax[nrow, ncol].set_ylabel(
+    r'$\langle C_{ik,t} \rangle$', fontsize=18, rotation=0, labelpad=25)
+ax[nrow, ncol].text(pos_l[0], pos_l[1], r'\textbf ' + letters[3], transform=ax[nrow, ncol].transAxes, fontsize=20, va='top', ha='right')
+
+nrow = 1
+ncol = 1
+ax[nrow, ncol].plot(betas, DPexp, color='k')
+ax[nrow, ncol].plot(betas, DP_t1, dashes=line[2], color=colors[2], lw=lws[2])
+ax[nrow, ncol].plot(betas, DP2_t, dashes=line[3], color=colors[3], lw=lws[3])
+ax[nrow, ncol].plot(betas, DP_t1_t, dashes=line[0], color=colors[0], lw=lws[0])
+ax[nrow, ncol].plot(betas, DP_t, dashes=line[1], color=colors[1], lw=lws[1])
+ax[nrow, ncol].plot(betas[np.argmax(DPexp)], [0.019], '*', ms=12, color='k')
+ax[nrow, ncol].plot(betas[np.argmax(DP_t1_t)], [0.019], '*', ms=12, color=colors[0])
+ax[nrow, ncol].plot(betas[np.argmax(DP_t)], [0.019], '*', ms=12, color=colors[1])
+ax[nrow, ncol].plot(betas[np.argmax(DP_t1)], [0.017], '*', ms=12, color=colors[2])
+ax[nrow, ncol].plot(betas[np.argmax(DP2_t)], [0.021], '*', ms=12, color=colors[3])
+ax[nrow, ncol].axis([np.min(betas), np.max(betas), 0, 0.022])
+ax[nrow, ncol].set_xlabel(r'$\tilde\beta$', fontsize=18)
+ax[nrow, ncol].set_ylabel(r'$\langle D_{il,t} \rangle$', fontsize=18, rotation=0, labelpad=25)
+ax[nrow, ncol].text(pos_l[0], pos_l[1], r'\textbf ' + letters[4], transform=ax[nrow, ncol].transAxes, fontsize=20, va='top', ha='right')
+
+nrow = 1
+ncol = 2
+ax[nrow, ncol].plot(betas, sigmaP_t1, dashes=line[2], color=colors[2], lw=lws[2])
+ax[nrow, ncol].plot(betas, sigmaP2_t, dashes=line[3],  color=colors[3], lw=lws[3])
+ax[nrow, ncol].plot(betas, sigmaP_t1_t, dashes=line[0], color=colors[0], lw=lws[0])
+ax[nrow, ncol].plot(betas, sigmaP_t, dashes=line[1], color=colors[1], lw=lws[1])
+ax[nrow, ncol].plot(betas, sigmaPexp, color='k')
+ax[nrow, ncol].plot(betas[np.argmax(sigmaPexp)], [110], '*', ms=12, color='k')
+ax[nrow, ncol].plot(betas[np.argmax(sigmaP_t1_t)], [ 120], '*', ms=12, color=colors[0])
+ax[nrow, ncol].plot(betas[np.argmax(sigmaP_t)], [ 100], '*', ms=12, color=colors[1])
+ax[nrow, ncol].plot(betas[np.argmax(sigmaP_t1)], [ 130], '*', ms=12, color=colors[2])
+ax[nrow, ncol].plot(betas[np.argmax(sigmaP2_t)], [ 120], '*', ms=12, color=colors[3])
+ax[nrow, ncol].plot([1,1], [0,135], lw=0.5, color='k')
+ax[nrow, ncol].axis([np.min(betas), np.max(betas), 0, 135])
+ax[nrow, ncol].set_xlabel(r'$\tilde\beta$', fontsize=18)
+ax[nrow, ncol].set_ylabel(r'$\mathrm{e}^{\sigma_t}$', fontsize=18, rotation=0, labelpad=25)
+ax[nrow, ncol].text(pos_l[0], pos_l[1], r'\textbf ' + letters[5], transform=ax[nrow, ncol].transAxes, fontsize=20, va='top', ha='right')
+plt.figlegend(loc='upper center', bbox_to_anchor=(0.5, 1.), borderaxespad=0, ncol=5)
+fig.tight_layout(h_pad=0.3, w_pad=0.7, rect=[0, 0, 1, 0.975])
+plt.savefig('img/results-reconstruted-transition.pdf', bbox_inches='tight')
--- reconstruction-Ising-problem.py
+++ reconstruction-Ising-problem.py
@@ -10,8 +10,11 @@ from mf_ising import mf_ising
 import numpy as np
 from matplotlib import pyplot as plt
 from matplotlib import cm
-import time
 
+def nsf(num, n=4):
+    """n-Significant Figures"""
+    numstr = ("{0:.%ie}" % (n-1)).format(num)
+    return float(numstr)
 
 size = 512
 
@@ -22,12 +25,12 @@ gamma2 = 0.1
 T = 128
 iu1 = np.triu_indices(size, 1)
 
-B = 61
+B = 201
+#B = 21
 
 
 betas = 1 + np.linspace(-1, 1, B) * 0.3
-#betas=np.array([1.0])
-#betas=np.array([0.97])
+
 
 filename1 = 'data/data-gamma1-' + str(gamma1) + '-gamma2-' + str(
         gamma2) + '-s-' + str(size) + '-R-' + str(R) + '-beta-1.0.npz'
@@ -36,10 +39,13 @@ s0 = data1['s0']
 del data1
 
 modes=['d','r']        # Direct and reconstruction modes
-mode = modes[1]
-for ib in range(len(betas)):
+mode = modes[0]
+#mode = modes[1]
+#for ib in range(len(betas)):
+for ib in range(123,200):
     beta_ref = round(betas[ib], 3)
-
+    print(beta_ref*1000)
+    exit()
 
     # Load data
     
@@ -88,7 +94,7 @@ for ib in range(len(betas)):
         I.J = JP_t1_t.copy()
     I.initialize_state(s0)
     for t in range(T):
-        print('beta',beta_ref,'P_t1_t_o2', str(t) + '/' + str(T),np.mean(I.m),np.mean(I.C[iu1]),np.mean(I.D))
+        print('beta',beta_ref,'P_t1_t_o2', str(t) + '/' + str(T),nsf(np.mean(I.m)), nsf(np.mean(I.C[iu1])), nsf(np.mean(I.D)))
         I.update_P_t1_t_o2()
     mP_t1_t_final = I.m
     CP_t1_t_final = I.C
@@ -104,7 +110,7 @@ for ib in range(len(betas)):
         I.J = JP_t.copy()
     I.initialize_state(s0)
     for t in range(T):
-        print('beta',beta_ref,'P_t_o2', str(t) + '/' + str(T),np.mean(I.m),np.mean(I.C[iu1]),np.mean(I.D))
+        print('beta',beta_ref,'P_t_o2', str(t) + '/' + str(T),nsf(np.mean(I.m)), nsf(np.mean(I.C[iu1])), nsf(np.mean(I.D)))
         I.update_P_t_o2()
     mP_t_final = I.m
     CP_t_final = I.C
@@ -118,11 +124,9 @@ for ib in range(len(betas)):
     elif mode =='r':
         I.H = HP2_t.copy()
         I.J = JP2_t.copy()
-#    I.H = H.copy()
-#    I.J = J.copy()
     I.initialize_state(s0)
     for t in range(T):
-        print('beta',beta_ref,'P2_t_o2', str(t) + '/' + str(T),np.mean(I.m),np.mean(I.C[iu1]),np.mean(I.D))
+        print('beta',beta_ref,'P2_t_o2', str(t) + '/' + str(T),nsf(np.mean(I.m)), nsf(np.mean(I.C[iu1])), nsf(np.mean(I.D)))
         I.update_P2_t_o2()
     mP2_t_final = I.m
     CP2_t_final = I.C
@@ -138,7 +142,7 @@ for ib in range(len(betas)):
         I.J = JP_t1.copy()
     I.initialize_state(s0)
     for t in range(T):
-        print('beta',beta_ref,'P_t1_o1', str(t) + '/' + str(T),np.mean(I.m),np.mean(I.C[iu1]),np.mean(I.D))
+        print('beta',beta_ref,'P_t1_o1', str(t) + '/' + str(T),nsf(np.mean(I.m)), nsf(np.mean(I.C[iu1])), nsf(np.mean(I.D)))
         I.update_P_t1_o1()
     mP_t1_final = I.m
     CP_t1_final = I.C
@@ -146,7 +150,7 @@ for ib in range(len(betas)):
 
     # Save results to file
 
-    filename = 'data/results/transition_'+mode+'_' + str(int(beta_ref * 100)) +'_R_' + str(R) +'.npz'
+    filename = 'data/reconstruction/transition_'+mode+'_' + str(int(round(beta_ref * 1000))) + '_R_' + str(R) +'.npz'
     np.savez_compressed(filename,
                         mP_t1_t=mP_t1_t_final,
                         mP_t=mP_t_final,
@@ -161,95 +165,3 @@ for ib in range(len(betas)):
                         DP_t1=DP_t1_final,
                         DP2_t=DP2_t_final)
 
-#    # Plot results
-
-#    steps = np.arange(T + 1)
-
-#    cmap = cm.get_cmap('inferno_r')
-#    colors = []
-#    for i in range(4):
-#        colors += [cmap((i + 0.5) / 4)]
-#    labels = [r'P[$t-1,t$]', r'P[$t$]', r'P[$t-1$]', r'P2[$t$]']
-
-
-#    fig, ax = plt.subplots(1, 1, figsize=(5, 4))
-#    plt.plot(steps, EmP_t1_t, 'b', label=labels[0])
-#    plt.plot(steps, EmP_t, 'g', label=labels[1])
-#    plt.plot(steps, EmP_t1, 'm', label=labels[2])
-#    plt.plot(steps, EmP2_t, 'r', label=labels[3])
-#    plt.title(r'$\beta/\beta_c=' + str(beta) + r'$', fontsize=18)
-#    plt.xlabel(r'$t$', fontsize=18)
-#    plt.ylabel(r'$MSE[\textbf{m}_t]$', fontsize=18, rotation=0, labelpad=30)
-#    plt.legend()
-#    # plt.savefig('img/error_m-beta_' + str(int(beta * 10)) +
-#    #            '.pdf', bbox_inches='tight')
-
-
-#    fig, ax = plt.subplots(1, 1, figsize=(5, 4))
-#    plt.plot(steps, ECP_t1_t, 'b', label=labels[0])
-#    plt.plot(steps, ECP_t, 'g', label=labels[1])
-#    plt.plot(steps, ECP_t1, 'm', label=labels[2])
-#    plt.plot(steps, ECP2_t, 'r', label=labels[3])
-#    plt.title(r'$\beta/\beta_c=' + str(beta) + r'$', fontsize=18)
-#    plt.xlabel(r'$t$', fontsize=18)
-#    plt.ylabel(r'$MSE[\textbf{C}_t]$', fontsize=18, rotation=0, labelpad=30)
-#    plt.legend()
-#    # plt.savefig('img/error_C-beta_' + str(int(beta * 10)) +
-#    #            '.pdf', bbox_inches='tight')
-
-#    fig, ax = plt.subplots(1, 1, figsize=(5, 4))
-#    plt.plot(steps, EDP_t1_t, 'b', label=labels[0])
-#    plt.plot(steps, EDP_t, 'g', label=labels[1])
-#    plt.plot(steps, EDP_t1, 'm', label=labels[2])
-#    plt.plot(steps, EDP2_t, 'r', label=r'P[D]')
-#    plt.title(r'$\beta/\beta_c=' + str(beta) + r'$', fontsize=18)
-#    plt.xlabel(r'$t$', fontsize=18)
-#    plt.ylabel(r'$MSE[\textbf{D}_t]$', fontsize=18, rotation=0, labelpad=30)
-#    plt.legend()
-#    # plt.savefig('img/error_D-beta_' + str(int(beta * 10)) +
-#    #            '.pdf', bbox_inches='tight')
-
-#    fig, ax = plt.subplots(1, 1, figsize=(5, 4))
-#    plt.plot(steps, mP_t1_t_mean, 'v', color=colors[0], ms=3, label=labels[0])
-#    plt.plot(steps, mP_t_mean, 's', color=colors[1], ms=3, label=labels[1])
-#    plt.plot(steps, mP_t1_mean, 'd', color=colors[2], ms=3, label=labels[2])
-#    plt.plot(steps, mP2_t_mean, 'o', color=colors[3], ms=3, label=labels[3])
-#    plt.plot(steps, mPexp_mean, 'k', label=r'$P$')
-#    plt.title(r'$\beta/\beta_c=' + str(beta) + r'$', fontsize=18)
-#    plt.xlabel(r'$t$', fontsize=18)
-#    plt.ylabel(r'$\langle m_{i,t} \rangle$', fontsize=18, rotation=0, labelpad=15)
-#    plt.legend()
-##    plt.savefig('img/evolution_m-beta_' + str(int(beta * 100)) +
-##                '.pdf', bbox_inches='tight')
-
-
-#    fig, ax = plt.subplots(1, 1, figsize=(5, 4))
-#    plt.plot(steps, CP_t1_t_mean, 'v', color=colors[0], ms=3, label=labels[0])
-#    plt.plot(steps, CP_t_mean, 's', color=colors[1], ms=3, label=labels[1])
-#    plt.plot(steps, CP_t1_mean, 'd', color=colors[2], ms=3, label=labels[2])
-#    plt.plot(steps, CP2_t_mean, 'o', color=colors[3], ms=3, label=labels[3])
-#    plt.plot(steps, CPexp_mean, 'k', label=r'$P$')
-#    plt.title(r'$\beta/\beta_c=' + str(beta) + r'$', fontsize=18)
-#    plt.xlabel(r'$t$', fontsize=18)
-#    plt.ylabel(r'$\langle C_{ik,t} \rangle$', fontsize=18, rotation=0, labelpad=15)
-#    plt.legend(loc='lower right')
-#    # plt.axis([0,T,0,1])
-##    plt.savefig('img/evolution_C-beta_' + str(int(beta * 100)) +
-##                '.pdf', bbox_inches='tight')
-
-#    fig, ax = plt.subplots(1, 1, figsize=(5, 4))
-#    plt.plot(steps, DP_t1_t_mean, 'v', color=colors[0], ms=3, label=labels[0])
-#    plt.plot(steps, DP_t_mean, 's', color=colors[1], ms=3, label=labels[1])
-#    plt.plot(steps, DP_t1_mean, 'd', color=colors[2], ms=3, label=labels[2])
-#    plt.plot(steps, DP2_t_mean, 'o', color=colors[3], ms=3, label=labels[3])
-#    plt.plot(steps, DPexp_mean, 'k', label=r'$P$')
-#    plt.title(r'$\beta/\beta_c=' + str(beta) + r'$', fontsize=18)
-#    plt.xlabel(r'$t$', fontsize=18)
-#    plt.ylabel(r'$\langle D_{il,t} \rangle$', fontsize=18, rotation=0, labelpad=15)
-#    plt.legend(loc='lower right')
-#    # plt.axis([0,T,0,1])
-##    plt.savefig('img/evolution_D-beta_' + str(int(beta * 100)) +
-##                '.pdf', bbox_inches='tight')
-#    
-#    #Close figures
-#    plt.close('all') 
