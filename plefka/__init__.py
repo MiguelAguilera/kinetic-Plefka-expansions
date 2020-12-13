727eb8942eeeedf1e5c47c0ec7b7ef513645fa01
MiguelAguilera <miguel@maguilera.net>
2020-12-13 21:19:17 +0100

    project structure

---
+++ plefka/__init__.py
@@ -0,0 +1,4 @@
+# __init__.py
+from .mf_ising import mf_ising
+from .plefka_functions import update_m_P_t1_t_o2, update_D_P_t1_t_o2, update_m_P_t_o2, update_D_P_t_o2
+from .plefka_functions import update_m_P_t1_o1, update_D_P_t1_o1, update_D_P2_t_o2
